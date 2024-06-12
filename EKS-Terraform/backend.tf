87c812eb70791d3f75b354ac186407954c28abb8
SahilBrightLink <sahil.patil@brightlinkinfotechnologies.com>
2024-06-12 17:26:26 +0530

    updates

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario-game" # Replace with your actual S3 bucket name
+    bucket = "mario-sp-game" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
